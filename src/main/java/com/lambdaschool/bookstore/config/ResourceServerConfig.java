134a6a6043595df1d4c8dd149efd89d1f28de0f9
tthompson082 <tthompson082@gmail.com>
2019-07-26 13:14:11 -0400

    :fire: Implement Authentication

--- src/main/java/com/lambdaschool/bookstore/config/ResourceServerConfig.java
+++ src/main/java/com/lambdaschool/bookstore/config/ResourceServerConfig.java
@@ -24,7 +24,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter
     public void configure(HttpSecurity http) throws Exception
     {
         // http.anonymous().disable();
-        http.authorizeRequests().antMatchers("/", "/h2-console/**", "/swagger-resources/**", "/swagger-resources/configuration/ui", "/swagger-resources/configuration/security", "/swagger-resource/**", "/swagger-ui.html", "/v2/api-docs", "/webjars/**", "/createnewuser", "/otherapis/**").permitAll().antMatchers("/users/**", "/oauth/revoke-token").authenticated().antMatchers("/roles/**").hasAnyRole("ADMIN", "USER", "DATA").antMatchers("/actuator/**").hasAnyRole("ADMIN").and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+        http.authorizeRequests().antMatchers("/books").hasAnyRole("USER", "DATA", "ADMIN").antMatchers("/authors").hasAnyRole("USER", "DATA", "ADMIN").antMatchers("/data").hasAnyRole("DATA", "ADMIN").antMatchers("/", "/h2-console/**", "/swagger-resources/**", "/swagger-resources/configuration/ui", "/swagger-resources/configuration/security", "/swagger-resource/**", "/swagger-ui.html", "/v2/api-docs", "/webjars/**", "/createnewuser", "/otherapis/**").permitAll().antMatchers("/users/**", "/oauth/revoke-token").authenticated().antMatchers("/roles/**").hasAnyRole("ADMIN", "USER", "DATA").antMatchers("/actuator/**").hasAnyRole("ADMIN").and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
 
         // http.requiresChannel().anyRequest().requiresSecure();
         http.csrf().disable();
