179a3fd557c4e7052b2fb0d6a5495a9a4e20d723
tthompson082 <tthompson082@gmail.com>
2019-07-26 11:29:18 -0400

    :label: Create Author and Book Service

---
+++ src/main/java/com/lambdaschool/bookstore/services/BookService.java
@@ -0,0 +1,17 @@
+package com.lambdaschool.bookstore.services;
+
+import com.lambdaschool.bookstore.models.Book;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+
+public interface BookService
+{
+    ArrayList<Book> findAll(Pageable pageable);
+
+    Book update(Book book, long id);
+
+    Book save(Book book);
+
+    void delete(long id);
+}
